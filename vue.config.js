5fef8b435c79ac28c32257014f459084684b8d3e
yuandaliu <6503599+yuandaliu@user.noreply.gitee.com>
2020-12-22 14:35:07 +0800

    修改结构

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    // 部署生产环境下的URL：可对当前环境进行区分，baseUrl 从Vue Cli 3.3 起已经弃用，要使用publicPath
-    // baseUrl: process,env.NODE.ENV === 'production' ? './' : '/'
-    publicPath: process.env.NODE_ENV === 'production' ? '/public' : './',
     devServer: {
         port: 1437
     },
